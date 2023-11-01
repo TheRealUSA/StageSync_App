15e5532a723c37a4e57c92db96f0b350b605d7ff
JosephIvan1406Fost <ivanalvarado1406@gmail.com>
2023-11-01 15:40:18 -0600

    vistas conectadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query'; // Importa QueryClient y QueryClientProvider
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
+import Body from './components/bodyFooter/body';
+import Footer from './components/footer/footer';
+
+// Importa tus componentes y pantallas
 import Inicio from './Screens/Inicio';
 import Musicos from './Screens/Musicos';
 import Oportunidades from './Screens/Oportunidades';
@@ -6,64 +14,41 @@ import Precios from './Screens/Precios';
 import InicioSesion from './Screens/InicioSesion';
 import Registrarse from './Screens/Registrarse';
 import RecuperarContrasena from './Screens/RecuperarContrasena';
-
 import Dashboard from './Dashboard/Dashboard';
-
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-
-import Navbar from './components/navbar/navbar';
-import Body from './components/bodyFooter/body';
-import Footer from './components/footer/footer';
-//import de error de link
-import Error404 from './Screens/Error404';
-
-//import de los datatable
 import Home from './Screens/Admin/Home';
 import DataTable from './Screens/Admin/DTmusicos';
-//CrudsMusico
 import AgregarMusico from './Dashboard/Cruds/Musicos/AgregarMusico';
 import VerMusico from './Dashboard/Cruds/Musicos/VerMusico';
 import EditarMusico from './Dashboard/Cruds/Musicos/EditarMusico';
+import Error404 from './Screens/Error404';
+
+const queryClient = new QueryClient(); // Crea una instancia del QueryClient
 
 function App() {
-  return ( <div className='flex flex-col'>
-<BrowserRouter>
-
-<Routes>
-  <Route path='/' element={<div> <Navbar/> <Inicio/> <Body/> <Footer/></div>}/>
-
-  <Route path='/Musicos' element={<div> <Navbar/> <Musicos/> <Body/> <Footer/> </div>}/>
-
-  <Route path='/Oportunidades' element={<div> <Navbar/> <Oportunidades/> <Body/> <Footer/> </div>}/>
-
-  <Route path='/Precios' element={<div> <Navbar/> <Precios/> <Body/>  <Footer/> </div>}/>
-
-  <Route path='/InicioSesion' element={<InicioSesion/>}/>
-
-  <Route path='/Registrarse' element={<Registrarse/>}/>
-
-  <Route path='/RecuperarContraseña' element={<RecuperarContrasena/>}/>
-
-    <Route path='/Dashboard' element={<Dashboard/>}>
-
-     <Route index element={<Home/>}/>
-
-     <Route path='Musico' element={<DataTable/>} />
-     
-     <Route path='AgregarMusico' element={<AgregarMusico/>} />
-
-     <Route path='VerMusico' element={<VerMusico/>} />
-
-     <Route path='EditarMusico' element={<EditarMusico/>} />
-
-    </Route>
-
-    <Route path='*' element={<Error404/>}/>
-
-</Routes>
-</BrowserRouter>
-
-  </div>
+  return (
+    <div className='flex flex-col'>
+      <Router>
+        <QueryClientProvider client={queryClient}> {/* Proporciona el QueryClient en el ámbito de tu aplicación */}
+          <Routes>
+            <Route path='/' element={<div> <Navbar/> <Inicio/> <Body/> <Footer/></div>}/>
+            <Route path='/Musicos' element={<div> <Navbar/> <Musicos/> <Body/> <Footer/> </div>}/>
+            <Route path='/Oportunidades' element={<div> <Navbar/> <Oportunidades/> <Body/> <Footer/> </div>}/>
+            <Route path='/Precios' element={<div> <Navbar/> <Precios/> <Body/>  <Footer/> </div>}/>
+            <Route path='/InicioSesion' element={<InicioSesion/>}/>
+            <Route path='/Registrarse' element={<Registrarse/>}/>
+            <Route path='/RecuperarContraseña' element={<RecuperarContrasena/>}/>
+            <Route path='/Dashboard' element={<Dashboard/>}>
+              <Route index element={<Home/>}/>
+              <Route path='Musico' element={<DataTable/>} />
+              <Route path='AgregarMusico' element={<AgregarMusico/>} />
+              <Route path='VerMusico' element={<VerMusico/>} />
+              <Route path='EditarMusico' element={<EditarMusico/>} />
+            </Route>
+            <Route path='*' element={<Error404/>}/>
+          </Routes>
+        </QueryClientProvider>
+      </Router>
+    </div>
   );
 }
 
--- src/Screens/Oportunidades.jsx
+++ src/Screens/Oportunidades.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { useQuery } from 'react-query';
+import { getAllProfileBusinesses } from '../Services/profilebusinesses.ts';
 
 const Oportunidades = () => {
-    return <div>Oportunidades</div>;
+    const { data: profiles, error, isLoading } = useQuery('Profile', () => getAllProfileBusinesses());
+
+    if (isLoading) {
+        return <div>Cargando...</div>;
+    }
+
+    if (error) {
+        return <div>Error al cargar los datos: {error.message}</div>;
+    }
+
+    return (
+        <div>
+            <h2>Perfiles de Negocio</h2>
+            <ul>
+                {profiles.map((profile) => (
+                    <li key={profile.id}>
+                        <div>
+                            <h3>{profile.business_name}</h3>
+                            <p>Owner: {profile.owner_name} {profile.owner_lastname1} {profile.owner_lastname2}</p>
+                            <p>Phone Number: {profile.phone_number}</p>
+                            <p>Location: {profile.city}, {profile.state}</p>
+                            <p>Address: {profile.street}, {profile.address}</p>
+                            <p>Days of the Week: {profile.days_of_the_week}</p>
+                            <p>Opening Time: {profile.openingTime}</p>
+                            <p>Closing Time: {profile.closingTime}</p>
+                            <p>About: {profile.about}</p>
+                            <p>Facebook: <a href={profile.facebook}>{profile.facebook}</a></p>
+                            <p>Instagram: <a href={profile.instagram}>{profile.instagram}</a></p>
+                            <img src={profile.avatar_route} alt={profile.avatar_filename} />
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Oportunidades;
