7a83f247a74e87b5f7d8bbf1d3b2225deb5cab2a
TheRealUSA <gerald.gonzalez.valdes@est.una.ac.cr>
2023-11-18 15:50:52 -0600

    Imagen Error404

--- src/Screens/Error404.jsx
+++ src/Screens/Error404.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Error404 = () => {
-    return <div>Error404</div>;
+  return (
+    <div className="flex items-center justify-center h-screen bg-[#5C1044]">
+      <img
+        src="./Img/Error404.webp"
+        alt="Descripción de la imagen"
+        className="max-w-full max-h-full"
+      />
+    </div>
+  );
 };
 
 export default Error404;
+
+
